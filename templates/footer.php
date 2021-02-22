53d25523f38f8103a245e9a20777245c1fe6d873
RaineRaine <43575887+RaineRaine@users.noreply.github.com>
2021-02-22 18:25:09 +0800

    Add files via upload

---
+++ templates/footer.php
@@ -0,0 +1,4 @@
+<h1>shjkadfbhjlasbf</h1>
+
+        </body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>  
+    <meta charset="UTF-8">
+    <title>title</title>
+	<link rel="stylesheet" href="<?php echo base_url("/assets/css/header.css"); ?>" />
+	<script src="https://kit.fontawesome.com/c2b30d2943.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+  <nav>
+  		<div class="container"> 
+  			<ul>
+  				<li>
+  					<a>Home</a>
+  				</li>
+  				<li>
+  					<a>About Us</a>
+  				</li>
+  				<li>
+  					<a>Our Framework</a>
+  				</li>
+  				<li>
+  					<a>Rated Hotels</a>
+  				</li>
+  				<li>
+  					<a>Partners</a>
+  				</li>
+  				<li>
+  					<a>Assessment</a>
+  				</li>
+  				<li>
+  					<a>Contact Us</a>
+  				</li>
+  				<li>
+  					<a>News</a>
+  				</li>
+  				<li>
+  					<a>FAQ</a>
+  				</li>
+  			</ul>
+  		</div>
+  	</nav> 
+  </div>
+</nav>
