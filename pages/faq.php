55fa9ea0b43b2f82f83d9e4bf4ebf451c0b15692
RaineRaine <43575887+RaineRaine@users.noreply.github.com>
2021-02-27 02:35:00 +0800

    WIP FAQ Page

---
+++ pages/faq.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>  
+    <meta charset="UTF-8">
+    <title>title</title>
+	<link rel="stylesheet" href="<?php echo base_url("/assets/css/faq.css"); ?>" />
+  <script type = 'text/javascript' src = "<?php echo base_url(); 
+         ?>/assets/js/faq.js"></script> 
+
+  </head>
+  <body>
+    <div class="bannerDiv">
+      <h1>THE DISASTER RESILIENCE RATING FOR HOTELS</h1>
+    </div>
+    <div class="subbannerDiv">
+      <p>Because RESILIENCE is everybody's Business.</p>
+    </div>
+
+    <h2 style="margin: 15px;">Frequently asked Questions</h2>
+
+    <!-- What is the DRRH? -->
+
+    <div class="popup" onclick="showPopup()">What is the Disaster Resilience Rating for Hotels?
+       <div class="popuptext" id="myPopup">
+         
+         <h3> What is the Disaster Resilience Rating for Hotels?</h3>
+         The DRRH is a thesis (capstone) project by a group of graduate students from the Asian Institute of Management (AIM), centered on co-creating a disaster resilience rating together with hotel industry associations/groups that will be shared publicly via Online Travel Agents (OTAs) and other media platforms as a way of gaining confidence from the general public.
+        </div>
+    </div>
+  </body>
+</html>
