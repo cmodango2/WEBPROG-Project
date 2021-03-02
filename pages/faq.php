e81e17ae04c48c7fe16354547bc38b2199629c49
RaineRaine <43575887+RaineRaine@users.noreply.github.com>
2021-03-03 00:24:44 +0800

    Update faq.php
    Updated the FAQ php

--- pages/faq.php
+++ pages/faq.php
@@ -2,30 +2,203 @@
 <html>
 <head>  
     <meta charset="UTF-8">
-    <title>title</title>
+    <title>FAQ</title>
 	<link rel="stylesheet" href="<?php echo base_url("/assets/css/faq.css"); ?>" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script type = 'text/javascript' src = "<?php echo base_url(); 
          ?>/assets/js/faq.js"></script> 
 
   </head>
-  <body>
+  <body onLoad="gridView();">
+    
     <div class="bannerDiv">
       <h1>THE DISASTER RESILIENCE RATING FOR HOTELS</h1>
     </div>
     <div class="subbannerDiv">
       <p>Because RESILIENCE is everybody's Business.</p>
     </div>
-
+    
     <h2 style="margin: 15px;">Frequently asked Questions</h2>
+    
+    <form class="example" action="action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit"><i class="fa fa-search"></i></button>
+    </form>
 
-    <!-- What is the DRRH? -->
-
-    <div class="popup" onclick="showPopup()">What is the Disaster Resilience Rating for Hotels?
-       <div class="popuptext" id="myPopup">
-         
-         <h3> What is the Disaster Resilience Rating for Hotels?</h3>
-         The DRRH is a thesis (capstone) project by a group of graduate students from the Asian Institute of Management (AIM), centered on co-creating a disaster resilience rating together with hotel industry associations/groups that will be shared publicly via Online Travel Agents (OTAs) and other media platforms as a way of gaining confidence from the general public.
+    <!-- Start of first row -->
+    <div class="row">
+      
+      <div class="column">
+      <button type="button" class="collapsible">What is the Disaster Resilience Rating for Hotels?</button>
+          <div class="content">
+              <p><br>The DRRH is a thesis (capstone) project by a group of graduate students from the Asian Institute of Management (AIM), centered on co-creating a disaster resilience rating together with hotel industry associations/groups that will be shared publicly via Online Travel Agents (OTAs) and other media platforms as a way of gaining confidence from the general public.</p.>
+          </div>
         </div>
+  
+    <div class="column">
+      <button type="button" class="collapsible">What are the procedures in having a hotel rated?</button>
+          <div class="content"><br>
+              After registration, the hotel will be guided on the process of self-assessment, the natural starting point of the evaluation.
+
+              Once the self-assessment is completed and the documentary requirements have been submitted, a third party auditor will coordinate with the hotel for the actual field audit where evaluators will visit the hotel and validate the self-assessment.
+
+              The auditor will then submit the findings to the industry association who will serve as the final approver of the rating.
+
+              Once approved, the hotel will be allowed to use the rating in all its promotional activities.
+          </div>
+        </div>
+      </div>
     </div>
+      
+    <!-- End of first row -->
+
+    <!-- Start of second row -->
+     <div class="row">
+      
+      <div class="column">
+      <button type="button" class="collapsible">How will the rating measure the hotel's level of disaster resilience?</button>
+          <div class="content">
+              <p><br>The DRRH follows the Resilience Programming Framework (RPF) created by Resilient.PH. The rating will evaluate the management practices — Asset Protection and Risk Management (APRM), Business Continuity Management (BCM), Disaster Risk Reduction (DRR), 
+              Emergency, Crisis and Opportunity Management (ECOM) and Risk/Crisis Communication (R/CCOMM) by reviewing existing policies, programs, systems, trainings, and tools (including finances) of hotels and assigning scores based on resilient level criteria - 
+              base (for local industry practices/compliance driven practices) mid (international best practice/above compliance) and high (for proven effective novel practices/thought leadership).</p.>
+          </div>
+        </div>
+  
+    <div class="column">
+      <button type="button" class="collapsible">What does the hotel need to provide/submit to be rated?</button>
+          <div class="content"><br>
+              As the critical areas for review includes policies, programs, systems, training, and tools (including finances), the rating will require hotels to show proof of the elements of the RPF in these areas.
+
+              Description of training programs for safety, company policy on disaster risk reduction, photos of health and safety features and resources, etc. are some of the examples of documents that will be asked of hotels undergoing the rating.
+          </div>
+        </div>
+      </div>
+    </div>
+      
+    <!-- End of second row -->
+
+    <!-- Start of third row -->
+    <div class="row">
+      
+      <div class="column">
+      <button type="button" class="collapsible">How will the hotel benefit from the rating?</button>
+          <div class="content">
+              <p><br>With the DRRH, hotels will be able to identify vulnerable components of their operations which they can work on to build their overall resilience.
+              More importantly, the rating will show the hotel’s  commitment to ensuring safety and wellbeing of guests and employees before, during, and after any hazard event which will add to the positive perception/image of the hotel.
+              The survey we conducted among hotel goers show that guests are likely to choose a hotel that they perceive to be resilient.</p.>
+          </div>
+        </div>
+  
+    <div class="column">
+      <button type="button" class="collapsible">What will the auditors/inspectors do when conducting the rating?</button>
+          <div class="content"><br>
+              The third-party audit process has two components — the first part includes checking the completeness and validity of documents submitted by the hotel being audited. The self-assessment results will be reviewed following the criteria set by the DRRH.
+
+              The second part involves field auditors visiting the hotel and validating the documents in the self-assessment through actual inspection, interview with employees and guests, interview with local government agencies and the community.
+          </div>
+        </div>
+      </div>
+    </div>
+      
+    <!-- End of third row -->
+
+    <!-- Start of fourth row -->
+    <div class="row">
+      
+      <div class="column">
+      <button type="button" class="collapsible">How much will the rating cost?</button>
+          <div class="content">
+              <p><br>The cost that will be charged to the hotel is the conduct of the third-party audit.
+              Initial estimates show that this may run anywhere between Php50 - 75K excluding travel and accommodation expenses for field audit. </p.>
+          </div>
+        </div>
+  
+    <div class="column">
+      <button type="button" class="collapsible">Can the hotel suddenly back-out during the conduct of the rating? Will the payment be refunded?</button>
+          <div class="content"><br>
+          The rating will only commence after payment of audit fees and completion of the self-assessment.
+
+There is a time for completing the self-assessment as well as time period for the field audit and the final approval.
+
+Should a hotel wish to discontinue the rating process, after the self-assessment, it will have to formally advise the industry association, but the fee will not be refunded when the field audit has been conducted.
+          </div>
+        </div>
+      </div>
+    </div>
+      
+    <!-- End of fourth row -->
+
+     <!-- Start of fifth row -->
+     <div class="row">
+      
+      <div class="column">
+      <button type="button" class="collapsible">What is the process for getting a hotel rated?</button>
+          <div class="content">
+              <p><br>Subject to the directions of the hotel industry association, the DRRH may be available to both members and non-members.
+
+              Hotels who would like to get rated simply has to register via website or app (or via online forms).</p.>
+          </div>
+        </div>
+  
+    <div class="column">
+      <button type="button" class="collapsible">How long is the whole rating process?</button>
+          <div class="content"><p><br>The self-assessment is to be completed within one month. The first part of the third-party audit will begin as soon as the self-assessment has been submitted and will run for a maximum of one week.
+
+The field audit will be done after the desk review has been completed and is expected to last anywhere between 3 to 5 days. The findings of the third-party audit will be transmitted to the hotel industry association who will be given a maximum of two weeks to approve or reject the rating.
+
+If there are no questions about the audit findings, the final approval will be communicated two weeks after submission of the third-party audit findings.</p>
+          
+          </div>
+        </div>
+      </div>
+    </div>
+      
+    <!-- End of fifth row -->
+
+    <!-- Start of sixth row -->
+    <div class="row">
+      
+      <div class="column">
+      <button type="button" class="collapsible">What will happen after the field audit?</button>
+          <div class="content">
+              <p><br> The findings of the third-party audit will be sent to the hotel industry association for final approval.
+
+              Once approved, the hotel industry association will send out a certificate of rating approval which the hotel can share with their respective OTAs as proof of authenticity of the rating.
+
+              The same certification can also be used by the hotel in all their promotion and marketing activities.
+
+              Hotels that will receive a High (RRR) rating will be awarded the certificate via a live public event at their venue of choice. </p.>
+          </div>
+        </div>
+  
+    <div class="column">
+      <button type="button" class="collapsible">Where will the rating of the hotel be published?</button>
+          <div class="content"><p><br>The hotel can choose where their approved rating will be published.</p>
+          
+          </div>
+        </div>
+      </div>
+    </div>
+      
+    <!-- End of sixth row -->
+
+
+    
+  <script>
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
+  </script>
   </body>
 </html>
