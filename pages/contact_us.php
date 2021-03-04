f4914c9861de02101b9f40ef2e26c7d99dd8f9fa
jfchuaAPC <79006324+jfchuaAPC@users.noreply.github.com>
2021-03-04 17:17:18 +0800

    Add files via upload

---
+++ pages/contact_us.php
@@ -0,0 +1,48 @@
+<h1 id = "cnt">
+    
+    CONTACT US
+
+</h1>
+<div class = "contact_us_cont"> 
+    <div class = "leftcol_cts">
+        <div class="leftcol_cts_items">
+            <img id="fbb" src="../../img/facebook.png" alt="facebook">
+            <h2 class = "info">Manila, Philippines</h2>
+        </div>
+        <div class="leftcol_cts_items">
+            <img id="fbb" src="../../img/facebook.png" alt="facebook">
+            <h2 class = "info">+63 9231230132 </h2>
+        </div>  
+        <div class="leftcol_cts_items">
+            <img id="fbb" src="../../img/facebook.png" alt="facebook">
+            <h2 class = "info">info@hoteldrrating.com</h2>
+        </div>
+    </div>
+    
+    <div class="rightcol_cts"> 
+
+        <form>
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Name<br></label>
+                <input type="text" class="form-control" id="name" placeholder="Name">
+            </div>
+
+
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Email address<br></label>
+                <input type="email" class="form-control" id="email" placeholder="Email address">
+            </div>
+
+
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Concern<br></label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+
+        </form>
+
+    </div>
+</div>
