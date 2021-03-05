19bd9a69e72729c2ea3b09990f9600d63dd921d3
Cheska Janine Odango <56395143+cmodango2@users.noreply.github.com>
2021-03-05 12:42:38 +0800

    Create terms.php

---
+++ pages/terms.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>HOME</title>
+        <link rel="stylesheet" href="<?php echo base_url("/assets/css/terms.css"); ?>"/>
+        <link rel="stylesheet" href="<?php echo base_url("/assets/bootstrap/css/bootstrap.css"); ?>"/>
+        <script src="https://unpkg.com/@popperjs/core@2"></script>      
+            <script type = 'text/javascript' src = "<?php echo base_url(); 
+                ?>/assets/bootstrap/js/bootstrap.js"></script>
+    </head>
+
+    <body>
+        <button type="button" class="framebutton btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Terms & Condition
+        </button>
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-scrollable">    
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Terms & Condition</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="banner">
+                            <h6>Terms on using/accessing the Websites' feature</h5> 
+                        </div>
+                        <h4>INTRODUCTION</h4>
+                        <p>
+                        <br>These Website Standard Terms and Conditions written on this webpage shall manage your use of our website, The Disaster Resilience Rating for Hotels accessible at http://www.hoteldrrating.com/.</br>
+                        <br>These Terms will be applied fully and affect to your use of this Website. By using this Website, you agreed to accept all terms and conditions written in here. You must not use this Website if you disagree with any of these Website Standard Terms and Conditions.</br>
+                        <br>Minors or people below 18 years old are not allowed to use this Website.</br></p>
+                        <hr>
+                        
+                        <h4>INTELLECTUAL PROPERTY RIGHTS</h4>
+                        <p>
+                        <br>Other than the content you own, under these Terms, DRRH and/or its licensors own all the intellectual property rights and materials contained in this Website.</br>
+                        <br>You are granted limited license only for purposes of viewing the material contained on this Website.</br></p>
+                        <hr>
+
+                        <h4>RESTRICTIONS</h4>
+                        <p>
+                        <br>You are specifically restricted from all of the following:</br>
+                        <ul>
+                            <li>publishing any Website material in any other media;</li>
+                            <li>selling, sublicensing and/or otherwise commercializing any Website material;</li>
+                            <li>publicly performing and/or showing any Website material;</li>
+                            <li>using this Website in any way that is or may be damaging to this Website;</li>
+                            <li>using this Website in any way that impacts user access to this Website;</li>
+                            <li>using this Website contrary to applicable laws and regulations, or in any way may cause harm to the Website, or to any person or business entity;</li>
+                            <li>engaging in any data mining, data harvesting, data extracting or any other similar activity in relation to this Website;</li>
+                            <li>using this Website to engage in any advertising or marketing.</li>
+                        </ul>  
+                            Certain areas of this Website are restricted from being access by you and DRRH may further restrict access by you to any areas of this Website, at any time, in absolute discretion. Any user ID and password you may have for this Website are confidential and you must maintain confidentiality as well.                                                
+                        </p>
+                        <hr>
+
+                        <h4>YOUR CONTENT</h4>
+                        <p>
+                        <br>In these Website Standard Terms and Conditions, "Your Content" shall mean any audio, video text, images or other material you choose to display on this Website. By displaying Your Content, you grant DRRH a non-exclusive, worldwide irrevocable, sub licensable license to use, reproduce, adapt, publish, translate and distribute it in any and all media.</br>
+                        <br>Your Content must be your own and must not be invading any third-party’s rights. DRRH reserves the right to remove any of Your Content from this Website at any time without notice.</br></p>
+                        <hr>
+
+                        <h4>YOUR PRIVACY</h4>
+                        <p>Please read our <a href="views/pages/privacy.php">Privacy Policy.</a></p>
+                        <hr>
+
+                        <h4>NO WARRANTIES</h4>
+                        <p>This Website is provided "as is," with all faults, and DRRH express no representations or warranties, of any kind related to this Website or the materials contained on this Website. Also, nothing contained on this Website shall be interpreted as advising you.</p>
+                        <hr>
+
+                        <h4>LIMITATION OF LIABILITY</h4>
+                        <p>In no event shall DRRH , nor any of its officers, directors and employees, shall be held liable for anything arising out of or in any way connected with your use of this Website whether such liability is under contract. DRRH , including its officers, directors and employees shall not be held liable for any indirect, consequential or special liability arising out of or in any way related to your use of this Website.</p>
+                        <hr>
+
+                        <h4>IDENTIFICATION</h4>
+                        <p>You hereby indemnify to the fullest extent DRRH from and against any and/or all liabilities, costs, demands, causes of action, damages and expenses arising in any way related to your breach of any of the provisions of these Terms.</p>
+                        <hr>
+                        
+                        <h4>SEVERABILITY</h4>
+                        <p>If any provision of these Terms is found to be invalid under any applicable law, such provisions shall be deleted without affecting the remaining provisions herein.</p>
+                        <hr>
+                        
+                        <h4>VARIATION OF TERMS</h4>
+                        <p>DRRH is permitted to revise these Terms at any time as it sees fit, and by using this Website you are expected to review these Terms on a regular basis.</p>
+                        <hr>
+
+                        <h4>ASSIGNMENT</h4>
+                        <p>The DRRH is allowed to assign, transfer, and subcontract its rights and/or obligations under these Terms without any notification. However, you are not allowed to assign, transfer, or subcontract any of your rights and/or obligations under these Terms.</p>
+                        <hr>
+
+                        <h4>ENTIRE AGREEMENT</h4>
+                        <p>These Terms constitute the entire agreement between DRRH and you in relation to your use of this Website, and supersede all prior agreements and understandings.</p>
+                        <hr>
+
+                        <h4>GOVERNING LAW & JURISDICTION</h4>
+                        <p>These Terms will be governed by and interpreted in accordance with the laws of the State of ph, and you submit to the non-exclusive jurisdiction of the state and federal courts located in ph for the resolution of any disputes.</p>
+                    
+                        <input type="checkbox" id="accept" value="I accept the Terms & Condition">
+                        <label for="terms"> I accept the Terms & Condition</label><br>
+                    
+                    </div>
+
+            </div>
+        </div>
+
+
+
+
+
+
+    </body>
+</html>
