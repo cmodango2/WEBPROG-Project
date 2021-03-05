d564a08ec53e4be03880b8ee905721bbe41ca3fc
Cheska Janine Odango <56395143+cmodango2@users.noreply.github.com>
2021-03-05 12:59:25 +0800

    Create news.php

---
+++ pages/news.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>HOME</title>
+        <link rel="stylesheet" href="<?php echo base_url("/assets/css/news.css"); ?>"/> 
+        <script src="https://kit.fontawesome.com/ac984e1e41.js" crossorigin="anonymous"></script>
+    </head>
+
+    <body>
+        
+        <div class="bannerDiv">
+            <h1>THE DISASTER RESILIENCE RATING FOR HOTELS</h1>
+        </div>
+        
+        <div class="subbannerDiv">
+            <p>Because RESILIENCE is everybody's Business.</p>
+        </div>
+
+
+        <div class="page-wrapper">
+
+            <div class="post-slider">
+                <p class="updatetxt">Latest Updates</p>
+
+                <i class="fas fa-chevron-left prev"></i>
+                <i class="fas fa-chevron-right next"></i>
+
+                <div class="post-wrapper">
+                    <div class="post">
+                        <img src="../../img/news1.jpg" alt="" class="sliderimg">
+                        <div class="article-info">
+                            <h3><a href="https://gulfnews.com/world/inside-view-covid-19-quarantine-hotels-in-manila-1.1610887127905?slide=4">Inside view: COVID-19 quarantine hotels in Manila</a></h3>
+                            <i class="far fa-user"> Joy Hilotin</i>
+                            &nbsp;
+                            <i class="far fa-calendar"> January 17, 2021</i>
+                        </div>
+                    </div>
+
+                    <div class="post">
+                        <img src="../../img/public.jpg" alt="" class="sliderimg">
+                        <div class="article-info">
+                            <h3><a href="https://gulfnews.com/world/inside-view-covid-19-quarantine-hotels-in-manila-1.1610887127905?slide=4">Inside view: COVID-19 quarantine hotels in Manila</a></h3>
+                            <i class="far fa-user"> Joy Hilotin</i>
+                            &nbsp;
+                            <i class="far fa-calendar"> January 17, 2021</i>
+                        </div>
+                    </div>
+                   
+                    <div class="post">
+                        <img src="../../img/news1.jpg" alt="" class="sliderimg">
+                        <div class="article-info">
+                            <h3><a href="https://gulfnews.com/world/inside-view-covid-19-quarantine-hotels-in-manila-1.1610887127905?slide=4">Inside view: COVID-19 quarantine hotels in Manila</a></h3>
+                            <i class="far fa-user"> Joy Hilotin</i>
+                            &nbsp;
+                            <i class="far fa-calendar"> January 17, 2021</i>
+                        </div>
+                    </div>
+
+                    <div class="post">
+                        <img src="../../img/phil.jpg" alt="" class="sliderimg">
+                        <div class="article-info">
+                            <h3><a href="https://gulfnews.com/world/inside-view-covid-19-quarantine-hotels-in-manila-1.1610887127905?slide=4">Inside view: COVID-19 quarantine hotels in Manila</a></h3>
+                            <i class="far fa-user"> Joy Hilotin</i>
+                            &nbsp;
+                            <i class="far fa-calendar"> January 17, 2021</i>
+                        </div>
+                    </div>
+
+                    <div class="post">
+                        <img src="../../img/public.jpg" alt="" class="sliderimg">
+                        <div class="article-info">
+                            <h3><a href="https://gulfnews.com/world/inside-view-covid-19-quarantine-hotels-in-manila-1.1610887127905?slide=4">Inside view: COVID-19 quarantine hotels in Manila</a></h3>
+                            <i class="far fa-user"> Joy Hilotin</i>
+                            &nbsp;
+                            <i class="far fa-calendar"> January 17, 2021</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--END-->
+
+        <div class="newsbanner">
+            <h1>NEWS</h1>
+        </div>
+
+        <h4 style="padding-bottom: 30px; font-weight: 20px; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; text-align: center; color: black; padding-top: 30px; font-size:20px">Rating Disaster Resilience of Hotels</h4>
+
+        <div class="work-conatiner">
+            <div class="description-news">
+                <img src="../../img/news2.jpg" class="img-resp float-img left">
+                <br>
+                <p><strong>Now that restrictions are slowly being lifted, and some semblance of normalcy is beginning to come back, what should hotel owners and management do to win back guests while also preparing for the next global disaster?</strong></p>
+                <br>
+                <p>This COVID-19 pandemic is an unprecedented event that has led to massive unemployment and economic downturns worldwide. The hotel sector, which is highly dependent on tourists and travelers, has been severely affected. Now that restrictions are slowly being lifted, and some semblance of normalcy is beginning to come back, what should hotel owners and management do to win back guests while also preparing for the next global disaster? What can hotels do to become more disaster resilient?</p>
+                <br>
+                <p>A group of graduate students of the Executive Master in Disaster Risk and Crisis Management (AIM), studying disaster risks and crisis management from one of the country’s leading learning institutions, is taking on the challenge of driving resilience consciousness among hotelgoers.</p>
+                </br>
+        
+
+            <div class="description-news">
+                <img src="../../img/new3.jpg" class="img-resp float-img right">
+                <br>
+                <p>Their thesis project — the <strong>Disaster Resilience Rating for Hotels (DRRH)</strong>, is specifically designed to enable the accommodations industry to bounce back better after every disaster.</p>
+                <br>
+                <p>According to the group leader, <strong>Randell Kitt Botero</strong>, <em>“In today’s volatile, uncertain, complex, and ambiguous world, resilience has become a buzz word. While usually associated with disasters and crises, the term is now widely used across disciplines. That said, it has also come to mean different things to different people. Some equate resilience with preparedness. Others associate it with being able to respond and effectively manage hazard events. In the broader interpretation of the word, it means the ability to bounce back after a shock. But what does disaster resilience mean for the hotel industry? This is at the core of our thesis.”</strong></em></p>
+                <br>
+                <p>The group’s thesis project is anchored on the Resilience Programming Framework (RPF) developed by Resilient.PH. The framework integrates already existing principles Into the organization’s very DNA, thus reframing traditional approaches on the need to effectively and sustainably bounce back better.</p>
+                </br>
+
+
+            </div>
+
+         <!--JQuery-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
+        
+        <!--Slick Carousel-->
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+        <!--Custom Script-->
+        <script src = "assets/js/news.js"></script>
+
+
+    </body>
+</html>
+
+        
