be77cd40cae6de21a5441b1ba2ea72ad0a3c2b7b
jfchuaAPC <79006324+jfchuaAPC@users.noreply.github.com>
2021-03-06 20:31:19 +0800

    Add files via upload

--- templates/footer.php
+++ templates/footer.php
@@ -1,30 +1,43 @@
-<link rel="stylesheet" href="<?php echo base_url("/assets/css/footer.css"); ?>" />
-        <div class="container">
+        <div class="container-footer">
                 <div class="border">
                         <br>
                 </div>
 
+
                 <div class="row1">
-                        <div class="left-col">
+
+
+                        <div class="left-col-footer">
                                 <h1>DISASTER RESILIENCE RATING FOR HOTELS</h1>
                                 <h2>Manila, Philippines<br>+63 9231230132<br>info@hoteldrrating.com </h2>
                         </div>
-                        <div class="right-col">
-                                <img class="twt" src="../../img/twitter.png" alt="twitter">
+
+                        <div class="right-col-footer">
                                 <img class="fb" src="../../img/facebook.png" alt="facebook">
-                                <img class="ig" src="../../img/insta.png" alt="instagram">
+                                <img class="twt" src="../../img/twitter.png" alt="twitter">
+                                <img class="twt" src="../../img/google.png" alt="google">
                                 <img class="linked" src="../../img/linkedin.png" alt="linkedin">
                                 <img class="yout" src="../../img/youtube.png"alt="youtube">
+                                <img class="ig" src="../../img/insta.png" alt="instagram">   
+                                
                         </div>
+
+
                 </div>
+
                 <div class="row2">
-                        <div class="left-col">
+                        <div class="left-col-footer">
                                 <h1><br>QUICK LINKS</h1>
-                                <h2>About Us | News | FAQ</h2>
+                                <a>About Us | </a>
+                                <a>News | </a>
+                                <a>FAQ | </a>
                 </div>
+
                 <div class="copyright">
                         <h3>Copyright 2021</h3>
                 </div>
         </div>
+
+
         </body>
 </html>
