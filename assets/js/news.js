6a1dac925efda44f062cd8584044c0389250f53d
Cheska Janine Odango <56395143+cmodango2@users.noreply.github.com>
2021-03-06 21:15:32 +0800

    Add files via upload

---
+++ assets/js/news.js
@@ -0,0 +1,9 @@
+$('.post-wrapper').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: $('.next'),
+    prevArrow: $('.prev')
+
+  });
