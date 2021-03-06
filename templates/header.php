fc49cce19d1545851f2dfcdddb8b8dbcbe7c85fe
jfchuaAPC <79006324+jfchuaAPC@users.noreply.github.com>
2021-03-06 20:23:49 +0800

    Add files via upload

--- templates/header.php
+++ templates/header.php
@@ -2,24 +2,49 @@
 <html>
   <head>  
     <meta charset="UTF-8">
-    <title>title</title>
+    <title>Home</title>
+	<link rel="stylesheet" href="<?php echo base_url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css") ?>">
+	<link rel="stylesheet" href="<?php echo base_url("/assets/css/footer.css"); ?>" />
 	<link rel="stylesheet" href="<?php echo base_url("/assets/css/header.css"); ?>" />
-	<script src="https://kit.fontawesome.com/c2b30d2943.js" crossorigin="anonymous"></script>
+
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?php echo base_url("/assets/css/contact_us.css"); ?>" />
   </head>
+
+
   <body>
+	  
   <nav>
   		<div class="container"> 
   			<ul>
-				  <li><a href="*">Home</a></li>
-				  <li><a href="*">About Us</a></li>
-				  <li><a href="*">Our Framework</a></li>
-				  <li><a href="*">Rated Hotels</a></li>
-				  <li><a href="*">Partners</a></li>
-				  <li><a href="*">Assessment</a></li>
-				  <li><a href="*">Contact Us</a></li>
-				  <li><a href="*">News</a></li>
-				  <li><a href="*">Safety Information</a></li>
-				  <li><a href="*">FAQ</a></li>
+  				<li>
+  					<a>Home</a>
+  				</li>
+  				<li>
+  					<a>About Us</a>
+  				</li>
+  				<li>
+  					<a>Our Framework</a>
+  				</li>
+  				<li>
+  					<a>Rated Hotels</a>
+  				</li>
+  				<li>
+  					<a>Partners</a>
+  				</li>
+  				<li>
+  					<a>Assessment</a>
+  				</li>
+  				<li>
+  					<a>Contact Us</a>
+  				</li>
+  				<li>
+  					<a>News</a>
+  				</li>
+  				<li>
+  					<a>FAQ</a>
+  				</li>
   			</ul>
   		</div>
   	</nav> 
